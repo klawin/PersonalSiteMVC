a6380d2cd3295a2cec90dffb65be865a6c60e519
Kayla Nguyen <kaylannguyen@outlook.com>
2022-03-10 18:48:32 -0600

    identity samples and admin user acct

--- PersonalSiteMVC.UI.MVC/Startup.cs
+++ PersonalSiteMVC.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(PersonalSiteMVC.UI.MVC.Startup))]
 namespace PersonalSiteMVC.UI.MVC
 {
     public partial class Startup
