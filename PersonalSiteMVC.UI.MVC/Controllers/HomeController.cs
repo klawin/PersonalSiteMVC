bb9d7992c6c52a2bc41119d655aae105587b16f2
Kayla Nguyen <kaylannguyen@outlook.com>
2022-03-11 14:27:51 -0600

    Links and contact page

--- PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using PersonalSiteMVC.UI.MVC.Models;
 
 namespace PersonalSiteMVC.UI.MVC.Controllers
 {
@@ -41,11 +44,57 @@ namespace PersonalSiteMVC.UI.MVC.Controllers
         }
 
         [HttpGet]
+        public ActionResult Links()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have recieved an email from {cvm.FirstName} {cvm.LastName}.<br />" +
+                $"Subject: {cvm.Subject}<br />" +
+                $"Message: {cvm.Message}<br />" +
+                $"Please reply to {cvm.Email} with your response at your earliest convenience.";
+
+            MailMessage mm = new MailMessage("administrator@kaylawin.com", "kayla.nghi.nguyen@gmail.com", cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.kaylawin.com");
+
+            client.Credentials = new NetworkCredential("administrator@kaylawin.com", "P@ssw0rd");
+
+            client.Port = 8889;
+
+            try
+            {
+                //Attempt to send email
+                client.Send(mm);
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry, your request could not be completed at this time. Please try again later. If the issue persists, please contact your site admnistrator and provide the following information:<br />{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
         }
     }
 }
---
+++ PersonalSiteMVC.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="*Required")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage ="*Required")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage ="*Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage ="*Required")]
+        public string Subject { get; set; }
+
+
+        [Required(ErrorMessage ="*Required")]
+        public string Message { get; set; }
+    }
+}
