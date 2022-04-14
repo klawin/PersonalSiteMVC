5ef9159c8804ba2fa3cad6b04fa12236d13744b3
Kayla Nguyen <kaylannguyen@outlook.com>
2022-04-14 15:12:45 -0500

    random changes

---
+++ PersonalSiteMVC.UI.MVC/Scripts/custom.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $("#accordion").accordion();
+});
+
+/**
+   * Testimonials slider
+   */
+new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    }
+});
